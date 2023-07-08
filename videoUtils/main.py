56dd1a72412182da799c0a77efca3a1d749d8eb6
Adnan <adnann3077@gmail.com>
2023-07-08 17:31:55 +0530

    "Added pvleopard engine"
    This reverts commit 685490ff0eaa1e542e8e9664384975d64a9ece21.
    
    Hello
    
    -- INSERT --

--- videoUtils/main.py
+++
@@ -1,7 +0,0 @@
-import pvleopard
-
-access_key="IEgSOHOe/DMdOBcOyD/mP5bFZTNncQKFx4ITB7iqO24OP5pj3wbUfA=="
-leopard = pvleopard.create(access_key=access_key)
-
-transcript, words = leopard.process_file('./a1.mp3')
-print(words)
